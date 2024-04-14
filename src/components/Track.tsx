9774cfba88d350ab40130b60cc2d41367ab27a30
goendia <goendia@posteo.de>
2024-04-14 19:44:18 +0200

    Fix render on mouseover

--- src/components/Track.tsx
+++ src/components/Track.tsx
@@ -20,39 +20,49 @@ const Track = ({
   volume,
   reverb,
 }: Props) => {
-  const [mouseOver, setMouseOver] = useState<boolean>(false),
-    [buffer] = useState<Tone.ToneAudioBuffer>(
-      new Tone.ToneAudioBuffer(
-        audio,
-        // Set a callback which fires when the audio buffer is loaded
-        (buffer: Tone.ToneAudioBuffer) => onDuration(buffer.duration)
-      )
-    ),
-    player = useMemo(() => {
-      // Initialize the player
-      const player = new Tone.Player({
-        url: buffer,
-        loop: true,
-        mute: true,
-        volume,
-      })
-        .toDestination()
-        .sync()
-        .start(0);
+  const [mouseOver, setMouseOver] = useState<boolean>(false);
+  const [buffer, setBuffer] = useState<Tone.ToneAudioBuffer>();
 
-      // Activate reverb if reverb is set
-      if (reverb)
-        player.chain(
-          new Tone.Reverb({
-            wet: reverb,
-            decay: 1.5,
-            preDelay: 0.01,
-          }),
-          Tone.Destination
-        );
+  useEffect(() => {
+    const audioBuffer = new Tone.ToneAudioBuffer(
+      audio,
+      // Set a callback which fires when the audio buffer is loaded
+      (buffer: Tone.ToneAudioBuffer) => {
+        onDuration(buffer.duration);
+        setBuffer(buffer);
+      }
+    );
 
-      return player;
-    }, [audio, volume]);
+    return () => {
+      audioBuffer.dispose();
+    };
+  }, []); // Empty dependency array means this effect runs once on mount and clean up on unmount
+
+  const player = useMemo(() => {
+    // Initialize the player
+    const player = new Tone.Player({
+      url: buffer,
+      loop: true,
+      mute: true,
+      volume,
+    })
+      .toDestination()
+      .sync()
+      .start(0);
+
+    // Activate reverb if reverb is set
+    if (reverb)
+      player.chain(
+        new Tone.Reverb({
+          wet: reverb,
+          decay: 1.5,
+          preDelay: 0.01,
+        }),
+        Tone.Destination
+      );
+
+    return player;
+  }, [buffer, audio, volume]);
 
   // Pass through mute state
   useEffect(() => {
